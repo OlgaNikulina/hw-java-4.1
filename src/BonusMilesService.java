b36d185de6fb5808905b8cc6033d4f1d00d59149
OlgaNikulina <nikulinaov@inbox.ru>
2020-02-25 13:09:50 +0500

    src/ modified

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,7 @@ public class BonusMilesService {
     public int calculate(int cost) {
         int price = 20000;
         int rubles = 20;
-        int miles = price/ rubles;
+        int miles = cost/ rubles;
         return miles;
     }
 }
